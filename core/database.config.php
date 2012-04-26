eb583b0517813f3bd16b04236a34053e0bc2d3f6
Pierre Criulanscy <pcriulan@gmail.com>
2012-04-26 19:03:23 +0200

    Config tom serveur

--- core/database.config.php
+++ core/database.config.php
@@ -1,10 +1,5 @@
 <?php
-/*define('DB_HOST', 'db3030.1and1.fr');
+define('DB_HOST', 'db3030.1and1.fr');
 define('DB_LOGIN', 'dbo355807268');
 define('DB_PASSWORD', 'dixitdixit');
-define('DB_NAME', 'db355807268');*/
-define('DB_HOST', 'localhost');
-define('DB_LOGIN', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'dixit');
-define('DB_PREFIX', '');
+define('DB_NAME', 'db355807268');

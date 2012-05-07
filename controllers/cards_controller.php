372681d85192b0308b08bc55232f1a46ebdb00ab
Pierre Criulanscy <pcriulan@gmail.com>
2012-05-07 14:11:34 +0200

    Message "test"

--- controllers/cards_controller.php
+++ controllers/cards_controller.php
@@ -22,7 +22,7 @@ function generateDeck() {
 		$cardID = addCardIn($i, "$i.png");
 		addCardInDeck($cardID, 4);
 	}
-	echo 'okok';
+	echo 'ok';
 }
 
 function addCardInDB() {
--- views/pages/home.php
+++ views/pages/home.php
@@ -1,1 +1,2 @@
 <h1>Homepage</h1>
+<p>Test</p>

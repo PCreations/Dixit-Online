14e448a0182aeb5fdff509e33a7f61b6550e35b8
Pierre Criulanscy <pcriulan@gmail.com>
2012-05-25 18:20:22 +0200

    Edit database config

--- core/database.config.php
+++ core/database.config.php
@@ -1,9 +1,9 @@
 <?php
-/*define('DB_HOST', 'db3030.1and1.fr');
+define('DB_HOST', 'db3030.1and1.fr');
 define('DB_LOGIN', 'dbo355807268');
 define('DB_PASSWORD', 'dixitdixit');
-define('DB_NAME', 'db355807268');*/
-define('DB_HOST', 'localhost');
+define('DB_NAME', 'db355807268');
+/*define('DB_HOST', 'localhost');
 define('DB_LOGIN', 'root');
 define('DB_PASSWORD', 'flipos6929');
-define('DB_NAME', 'dixit');
+define('DB_NAME', 'dixit');*/

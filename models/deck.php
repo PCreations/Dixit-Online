3714d378033a3ad7abaa83f2da776213ec7f273b
Tom Duchene <matteo_fantasy@hotmail.fr>
2012-05-26 16:42:18 +0200

    ajout de l'action "récupérer les infos de tous les decks"

--- models/deck.php
+++ models/deck.php
@@ -8,5 +8,13 @@ function addDeck($userID, $deckName, $deckStatus) {
 	$query->execute(array('userID' => $userID,
 						'deckName' => $deckName,
 						'deckStatus' => $deckStatus));
-	
+}
+
+function getDecksInfos($fields = array('*')) {
+	global $db;
+	$fields = implode(',', $fields);
+
+	$query = $db->prepare('SELECT '.$fields.' 
+						FROM decks');
+	return $query->fetchAll(PDO::FETCH_ASSOC);
 }

a5ee8ea034ab793d4dc3bf47423977eec92f5485
Cecilia Lejeune <cecilia.lejeu@gmail.com>
2012-05-27 13:54:17 +0200

    Edit account.php - Suppression d'un ami

--- index.php
+++ index.php
@@ -28,7 +28,7 @@ $JS_FILES = array('jquery-1.7.2.min.js', 'script.js');
 /**
  * Tableau contenant la liste des fichiers .css à inclure
  */
-$CSS_FILES = array('style.css', 'test.css');
+$CSS_FILES = array('style_contact.css', 'style_home.css', 'style_users.css', 'style_games.css');
 
 
 require_once('core/core.php');
--- views/themes/default/layout.php
+++ views/themes/default/layout.php
@@ -1,11 +1,21 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html>
-	<head>
-		<meta charset="utf-8" />
-        <?php echo $cssList;?>
+    <head>
+		<meta charset="UTF-8"/>
         <?php echo $jsList;?>
+        <?php echo $cssList;?>
 		<title><?php echo $pageTitle;?></title>
-	</head>
+    </head>
+	
 	<body>
-		<?php echo $contentForLayout;?>
+		<header>
+			<img src="<?php echo IMG_DIR;?>titre.png">
+		</header>
+		<section>
+			<?php echo $contentForLayout;?>
+		</section>
+		<footer>
+			Dixit | Copyright 2012 |Tous droits réservés
+		</footer>
 	</body>
 </html>
